182d4ab07161da65050f48b19bdb1dbab087bc23
imheldermartins <helder.santos0209@gmail.com>
2026-05-07 00:53:52 -0300

    refactor: clean up app structure and update TypeScript configuration
    - Learning about state with `signal`
    - Manipulating template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  count = signal(0);
+
+  increment() {
+    this.count.update(c => c + 1)
+  }
 }
