bc7c7b0cb17f3181751b21bacf578d74dfb1c544
imheldermartins <helder.santos0209@gmail.com>
2026-05-07 21:38:39 -0300

    feat: enhance sign-in page with custom text-input component for email and password fields

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,23 +1,56 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { Typography } from '../typography/typography';
+import { Field, FormField } from '@angular/forms/signals';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'input[ui-input]',
-  template: `<ng-content />`,
-  host: {
-    '[class]': 'getHostsClasses()',
-  },
+  selector: 'text-input',
+  imports: [FormField, NgClass, Typography],
+  template: `
+    <div class="w-full flex flex-col gap-1 items-start">
+      @if (label()) {
+        <label [for]="id()" class="text-sm font-medium text-zinc-700">
+          {{ label() }}
+        </label>
+      }
+
+      <input
+        [type]="type()"
+        [id]="id()"
+        [placeholder]="placeholder()"
+        [formField]="field()"
+        class="w-full p-2 ring-1 rounded-lg outline-none transition-all"
+        [ngClass]="{
+          'ring-red-500 text-red-600 bg-red-50 focus:ring-red-600': hasError(),
+          'ring-slate-300 focus:ring-slate-500': !hasError(),
+        }"
+      />
+
+      @if (hasError()) {
+        @for (error of errors(); track error.kind) {
+          <span typography class="text-red-500">{{ error.message }}</span>
+        }
+      }
+    </div>
+  `,
 })
 export class Input {
-  variant = input<'default' | 'error'>('default');
+  type = input<'text' | 'number' | 'email' | 'password'>('text');
 
-  protected getHostsClasses() {
-    const base = 'p-2 ring-1 rounded-lg outline-none';
+  label = input<string>('');
+  placeholder = input<string>('');
 
-    const variants = {
-      default: 'ring-slate-300 focus:ring-slate-500',
-      error: 'ring-red-500 text-red-500',
-    };
+  field = input.required<Field<string>>();
 
-    return `${base} ${variants[this.variant()]}`;
-  }
+  id = input<string>(`input-${Math.random().toString(36).substring(2, 9)}`);
+
+  protected hasError = computed(() => {
+    const state = this.field()();
+    return state?.invalid() && (state.dirty() || state.touched());
+  });
+
+  protected errors = computed(() => {
+    const state = this.field()();
+    return state.errors() || [];
+  });
 }
